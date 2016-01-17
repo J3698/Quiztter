ad8635784096ef79d6be3a0078464ace35b331d4
J3698 <antiochsanders@gmail.com>
2016-01-16 20:14:55 -0500

    -imports in Play.java

--- quiztter/Play.java
+++ quiztter/Play.java
@@ -3,12 +3,12 @@ package quiztter;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.geom.Rectangle2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseWheelEvent;
+import java.awt.geom.Rectangle2D;
 
 import gfm.Game;
 import gfm.gamestate.GameState;
