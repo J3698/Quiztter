9d20e7723d092e8b93ca1220698560291a9b689f
J3698 <antiochsanders@gmail.com>
2016-01-18 23:12:51 -0500

    -pop words moved to PopularWordFinder.java

--- quiztter/GTwitter.java
+++ quiztter/GTwitter.java
@@ -311,148 +311,20 @@ public class GTwitter {
    }
 
 
-
-
-
    private static Question commonWordOfUserQuestion() throws TwitterException {
-      String handle = randomHandle();
-      String name = handle;
+      PopularTwitterUser user = randomUser();
+      String name = user.getName(twitter);
 
       String questionText = "Which word does " + name + " use most often?";
-      ArrayList<WordAndInt> popWords = mostPopularWords(handle);
+      String[] popWords = user.getPopularWords(twitter);
 
-      String ans0 = popWords.get(0).getWord();
-      String ans1 = popWords.get(1).getWord();
-      String ans2 = popWords.get(2).getWord();
-      String ans3 = popWords.get(3).getWord();
-
-      return new Question(questionText, "A", ans0, ans1, ans2, ans3);
+      return new Question(
+            questionText, "A", popWords[0], popWords[1], popWords[2], popWords[3]);
    }
-
-   private static ArrayList<String> makeSortedListOfWordsFromTweets(String twitterHandle) throws TwitterException {
-      ArrayList<Status> statuses = new ArrayList<Status>();
-      ArrayList<String> sortedTerms = new ArrayList<String>();
-
-      Paging page = new Paging(1, 200);
-      int p = 1;
-      while ( p <= 4 ) {
-         page.setPage(p);
-         statuses.addAll(twitter.getUserTimeline(twitterHandle, page));
-         p++;
-      }
-
-      //Makes a list of words from user timeline
-      for (Status status : statuses) {
-         String[]array = status.getText().split(" ");
-         for (String word : array) {
-            sortedTerms.add(removePunctuation(word).toLowerCase());
-         }
-      }
-
-      // Remove common English words, which are stored in commonWords.txt
-      sortedTerms = removeCommonEnglishWords(sortedTerms, twitterHandle);
-      sortAndRemoveEmpties(sortedTerms);
-      return sortedTerms;
-   }
-
-   private static String removePunctuation(String s) {
-      // things to delete
-      if ( s.length() != 0 && s.substring(0, 1).equals("@") ) { return ""; }
-      if ( s.length()!= 0 && s.substring(0, 1).equals("#") ) { return ""; }
-      if ( s.contains("www.") ) { return ""; }
-      if ( s.contains("http") ) { return ""; }
-      if ( s.length() <= 2) { return ""; }
-
-      String toReturn = "";
-      String punct = "`~!@#$%^&*()_+-={}|[]\\:\";'<>?,./";
-
-      for ( int i = 0; i < s.length(); i++ ) {
-         if ( !punct.contains(s.substring(i, i + 1)) ) {
-            toReturn += s.substring(i, i + 1);
-         }
-      }
-      return toReturn;
-   }
-
-
-   private static ArrayList<String> removeCommonEnglishWords (ArrayList<String> list, String twitterHandle) {
-      BufferedReader inFile = null;
-
-      try {
-         inFile = new BufferedReader(
-               new InputStreamReader(ClassLoader.getSystemResourceAsStream("./quiztter/commonWords.txt")));
-
-         String toRemove;
-         while ( (toRemove = inFile.readLine()) != null ) {
-            while ( list.contains(toRemove.toLowerCase()) ) {
-               list.remove(toRemove.toLowerCase());
-            }
-         }
-
-         while ( list.contains(twitterHandle.toLowerCase()) ) {
-            list.remove(twitterHandle.toLowerCase());
-         }
-      } catch (Exception e) {
-         e.printStackTrace();
-      } finally {
-         if ( inFile != null ) {
-            try {
-               inFile.close();
-            } catch (IOException e) {
-               e.printStackTrace();
-            }
-         }
-      }
-
-      return list;
-   }
-
-   private static void sortAndRemoveEmpties(ArrayList<String> sortedTerms) {
-      while ( sortedTerms.contains("") ) {
-         sortedTerms.remove("");
-      }
-      Collections.sort(sortedTerms);
-   }
-
-   public static ArrayList<WordAndInt> mostPopularWords(String twitterHandle) throws TwitterException {
-      ArrayList<String> sortedTerms = makeSortedListOfWordsFromTweets(twitterHandle);
-      ArrayList<WordAndInt> words = new ArrayList<WordAndInt>();
-
-      String word = null;
-      for ( int i = 0; i < sortedTerms.size(); i++ ) {
-         word = sortedTerms.get(i);
-         boolean added = false;
-
-         WordAndInt pair = null;
-         for ( int j = 0; j < words.size(); j++ ) {
-            pair = words.get(j);
-            if ( word.equals(pair.getWord()) ) {
-               pair.add();
-               added = true;
-               break;
-            }
-         }
-         if ( !added ) {
-            words.add(new WordAndInt(word, 1));
-         }
-      }
-      Collections.sort(words);
-      return words;
-   }
-
-
-
-
-
-
-
 
 
    public static final String consumerKey = "jaCh0s73zKRjebHlXRq6rCK85";
    public static final String consumerSecret = "e1xbrwOQZgkjGwPu4zxHxBaGfHKWLnhaaWqon30c6C8gJPOZFH";
-
-
-
 
 
    public static void disable() {
